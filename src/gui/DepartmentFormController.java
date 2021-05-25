ab6380da5bee647d3945302930dd8ddf133535c6
João Pedro Silva <joao.silva@conexasaude.com.br>
2021-05-25 10:21:22 -0300

    Passando um objeto Department para o formulario

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,67 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Department;
+
+public class DepartmentFormController implements Initializable {
+
+	private Department entity;
+	
+	@FXML
+	private TextField textId;
+	 
+	@FXML
+	private TextField textName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+	
+	public void initializeNodes() {
+		Constraints.setTextFieldInteger(textId);
+		Constraints.setTextFieldMaxLength(textName, 55);
+	}
+	
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		textId.setText(String.valueOf(entity.getId()));
+		textName.setText(entity.getName());
+		
+	}
+
+}
